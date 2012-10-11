9543d9f244263e3dbb49a29c126161fb8b57e592
Anna Vester <anna@idea-loop.com>
2012-10-10 21:21:29 -0500

    moved to localStorage, started work on time conversion

--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,18 @@
 'use strict';
 
 /* Services */
-angular.module('taskServices', ['ngResource']).
-    factory('Task', function($resource) {
-        return $resource('tasks/tasks.json', {}, {
-            query: {method: 'GET', params: {}, isArray:true }
-        });
-    });
+angular.module('taskServices', []).
+  factory('allTasks', function() {
+    var STORAGE_ID = 'tasks';
+
+    return {
+      get: function() {
+        return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+      },
+
+      put: function(tasks) {
+        localStorage.setItem(STORAGE_ID, JSON.stringify(tasks));
+      }
+
+    };
+});
