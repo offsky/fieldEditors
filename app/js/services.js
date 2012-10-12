f7607815cb32901fa25fe1c26ca0ff81e92b671d
Anna Vester <anna@idea-loop.com>
2012-10-11 22:10:00 -0500

    seeded localStorage

--- app/js/services.js
+++ app/js/services.js
@@ -4,7 +4,10 @@
 angular.module('taskServices', []).
   factory('allTasks', function() {
     var STORAGE_ID = 'tasks';
-
+    if (!localStorage.getItem(STORAGE_ID)) {
+      var sampleData = [{"title":"Sample Task 1","length":"30 minutes","startDate":"10/22/2012"},{"title":"Sample Task 1","length":"30 minutes","startDate":"10/22/2012"}];
+      localStorage.setItem(STORAGE_ID, JSON.stringify(sampleData));
+    }
     return {
       get: function() {
         return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
