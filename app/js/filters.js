62ec719b9d8cb873a6a4b7a9d472c82f622e4e90
Jake Olefsky <offsky@gmail.com>
2012-10-16 08:38:48 -0700

    Template for writing filters

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,3 +7,60 @@ filters.filter('checkmark', function() {
     return input ? '\u2713' : '\u2718';
   };
 });
+
+
+filters.filter('length', function() {
+  return function(input) {
+    return 'length';
+  };
+});
+
+filters.filter('percent', function() {
+  return function(input) {
+    return 'percent';
+  };
+});
+
+filters.filter('url', function() {
+  return function(input) {
+    return 'url';
+  };
+});
+
+filters.filter('rating', function() {
+  return function(input) {
+    return 'rating';
+  };
+});
+
+filters.filter('star', function() {
+  return function(input) {
+    return 'star';
+  };
+});
+
+filters.filter('phone', function() {
+  return function(input) {
+    return 'phone';
+  };
+});
+
+filters.filter('email', function() {
+  return function(input) {
+    return 'email';
+  };
+});
+
+
+filters.filter('icon', function() {
+  return function(input) {
+    return 'icon';
+  };
+});
+
+
+filters.filter('isbn', function() {
+  return function(input) {
+    return 'isbn';
+  };
+});
